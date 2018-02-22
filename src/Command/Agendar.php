d4eef2790a760e1785d247be19d46f89c24c3204
Emílio B. Pedrollo <emilio.pedrollo@chipinside.com.br>
2018-02-22 12:32:36 -0300

    Extends logic to load load.yml configuration file

--- src/Command/Agendar.php
+++ src/Command/Agendar.php
@@ -5,9 +5,12 @@ namespace AutoUFSM\Command;
 use Carbon\Carbon;
 use GuzzleHttp\Client;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Yaml\Exception\ParseException;
 use Symfony\Component\Yaml\Yaml;
 use Illuminate\Support\Collection;
 
@@ -15,6 +18,7 @@ class Agendar extends Command {
 
 
     protected $load;
+    protected $load_filename;
 
     protected function configure() {
         $this
@@ -33,22 +37,56 @@ class Agendar extends Command {
         ];
     }
 
-    protected function initialize(InputInterface $input, OutputInterface $output) {
-        $this->load = $input->getOption('load');
-        if ($this->load == null) {
+    protected function getRealPath($path): ?string {
+        if ($path === null) return null;
 
-            if (\Phar::running()){
-                $this->load = Yaml::parseFile(getcwd() . "/load.yml");
-            } else {
-                $this->load = Yaml::parseFile(__DIR__ . "/../../load.yml");
-            }
+        if (function_exists('posix_getuid') && strpos($path, '~') !== false) {
+            $info = posix_getpwuid(posix_getuid());
+            $path = str_replace('~', $info['dir'], $path);
+        }
 
+        return realpath($path);
+
+    }
+
+    protected function loadYaml(OutputInterface $output){
+        try {
+            $this->load = Yaml::parseFile($this->getRealPath($this->load_filename));
+        } catch (ParseException $e) {
+            $output->writeln("Could not parse load file ($this->load_filename), aborting: ".
+                $e->getMessage());
+            throw $e;
         }
 
     }
 
+    protected function initialize(InputInterface $input, OutputInterface $output) {
+
+        $file = $this->getRealPath($input->getOption('load')) ?: ((\Phar::running())?
+            getcwd() . "/load.yml" : __DIR__ . "/../../load.yml");
+
+        if (file_exists($file)) {
+            $this->load_filename = $file;
+        } else {
+            $output->writeln("Could not find a load.yml file");
+        }
+
+    }
+
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
+        if (!$this->load_filename){
+            /** @var QuestionHelper $helper */
+            $helper = $this->getHelper('question');
+            $question = new Question("Please provide the location of a valid load.yml file: ");
+
+            $this->load_filename = $helper->ask($input,$output,$question);
+        }
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output) {
 
+        $this->loadYaml($output);
 
         $agora = Carbon::now();
 
